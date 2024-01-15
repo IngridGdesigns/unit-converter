1a8684c20ca068203943b4cf423af576afa4c2d2
IngridGdesigns <garciai@protonmail.com>
2024-01-15 15:58:37 -0800

    added some brute force code WIP

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 
 /*
 To-do: 
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
 
-calculate measurements - 
-    1 meter = 3.281 feet
-    1 liter = 0.264 gallon
-    1 kilogram = 2.204 pound
 */
+
+let len = document.getElementById("length");
+
+let txt = "";
+function convert(item) {
+    if(item === length){
+        console.log("here is my length");
+        // len.textContent += `\n Something`
+        // 20 meters = 65.616 feet | 20 feet = 6.096 meters
+        const number = len.value;
+        len.textContent += `${number} meters = 11 feet | ${number} feet = 11 meters`;
+    }
+}
+
+convert(length);
