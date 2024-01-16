1872ae9e603bc0c62bd2fbfabdd6aa563442591e
IngridGdesigns <garciai@protonmail.com>
2024-01-15 17:10:21 -0800

    added more logic, WIP

--- index.js
+++ index.js
@@ -3,22 +3,34 @@
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
+\
+To-do: Calculate function for length, volume, mass
 */
 
-// 20 meters = 65.616 feet | 20 feet = 6.096 meters
 let lengthUnit = document.getElementById("length");
 let volumeUnit = document.getElementById("volume");
 let massUnit = document.getElementById("mass");
 const inputNum = document.getElementById("input-number");
 const btnConvert = document.getElementById("btn-convert");
 
-// let txt = "";
-function convert(number) {
-    
-        console.log("here is my num");
+
+function convert(number, measurement) {
+        if(!number){
+            number = 20; // placeholder
+        }
       
-        const l = `${number} meters = 11 feet | ${number} feet = 11 meters`;
-        return l;
+        if(measurement === lengthUnit){
+            const len = `${number} meters = 11 feet | ${number} feet = 11 meters`;
+            return len;
+        }
+        if(measurement === volumeUnit){
+            const vol = `${number} liters = 5.284 gallons | ${number} gallons = 75.708 liters`;
+            return vol;
+        }
+         if(measurement === massUnit){
+            const mass = `${number} kilos = 44.092 pounds | ${number} pounds = 9.072 kilos`;
+            return mass;
+        }
     
 }
 
@@ -26,7 +38,8 @@ btnConvert.addEventListener("click", () => {
     let stringNum = inputNum.value;
     let num = parseInt(stringNum);
     
-    lengthUnit.textContent = convert(num)
+    lengthUnit.textContent = convert(num, lengthUnit);
+    volumeUnit.textContent = convert(num, volumeUnit);
+    massUnit.textContent = convert(num, massUnit);
  
-})
-
+});
